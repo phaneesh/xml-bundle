33a22e2b5a29a5c78fdb2991fc9ede88574d1c5d
phaneesh <phaneesh.n@gmail.com>
2016-03-04 17:41:35 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,76 @@
+# Dropwizard XML Bundle [![Travis build status](https://travis-ci.org/phaneesh/xml-bundle.svg?branch=master)](https://travis-ci.org/phaneesh/mxl-bundle)
+
+This bundle adds XML request/response support for dropwizard.
+This bundle compiles only on Java 8.
+ 
+## Dependencies
+* XML Jackson Data format 2.6.3  
+
+## Usage
+The bundle adds XML support based on content negotiation. This makes it easier to switch between legacy friendly and readable protocols. 
+ 
+### Build instructions
+  - Clone the source:
+
+        git clone github.com/phaneesh/xml-bundle
+
+  - Build
+
+        mvn install
+
+### Maven Dependency
+Use the following repository:
+```xml
+<repository>
+    <id>clojars</id>
+    <name>Clojars repository</name>
+    <url>https://clojars.org/repo</url>
+</repository>
+```
+Use the following maven dependency:
+```xml
+<dependency>
+    <groupId>io.dropwizard.xml</groupId>
+    <artifactId>xml-bundle</artifactId>
+    <version>0.0.1</version>
+</dependency>
+```
+
+### Using MsgPack bundle
+
+#### Bootstrap
+```java
+    @Override
+    public void initialize(final Bootstrap...) {
+        bootstrap.addBundle(new XmlBundle());
+    }
+```
+
+#### Resource
+```java
+    @GET
+    @Produces({MediaType.APPLICATION_XML})
+    @Path("/example")
+    @Metered
+    public ExamplePojo example() {
+        return ExamplePojo("hello");
+    }
+```
+
+
+LICENSE
+-------
+
+Copyright 2016 Phaneesh Nagaraja <phaneesh.n@gmail.com>.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
---
+++ src/main/java/io/dropwizard/xml/XmlPackMessageBodyProvider.java
@@ -0,0 +1,83 @@
+/*
+ * Copyright 2016 Phaneesh Nagaraja <phaneesh.n@gmail.com>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.dropwizard.xml;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+import com.fasterxml.jackson.dataformat.xml.ser.ToXmlGenerator;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.Produces;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.ext.MessageBodyReader;
+import javax.ws.rs.ext.MessageBodyWriter;
+import javax.ws.rs.ext.Provider;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Type;
+
+/**
+ * @author phaneesh
+ */
+@Provider
+@Consumes(MediaType.APPLICATION_XML)
+@Produces(MediaType.APPLICATION_XML)
+public class XmlPackMessageBodyProvider<T> implements MessageBodyReader<T>, MessageBodyWriter<T> {
+
+    public static final XmlMapper objectMapper = new XmlMapper();
+
+    public XmlPackMessageBodyProvider() {
+        objectMapper.configure(SerializationFeature.INDENT_OUTPUT, true);
+        objectMapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        objectMapper.configure(SerializationFeature.WRITE_EMPTY_JSON_ARRAYS, true);
+        objectMapper.setSerializationInclusion(JsonInclude.Include.NON_EMPTY);
+        objectMapper.configure(ToXmlGenerator.Feature.WRITE_XML_1_1, true);
+    }
+
+    @Override
+    public boolean isReadable(Class<?> type, Type genericType, Annotation[] annotations, MediaType mediaType) {
+        return mediaType.isCompatible(MediaType.APPLICATION_XML_TYPE);
+    }
+
+    @Override
+    public boolean isWriteable(Class<?> type, Type genericType,
+                               Annotation[] annotations, MediaType mediaType) {
+        return mediaType.isCompatible(MediaType.APPLICATION_XML_TYPE);
+    }
+
+    @Override
+    public T readFrom(Class<T> type, Type genericType, Annotation[] annotations, MediaType mediaType, MultivaluedMap<String, String> httpHeaders, InputStream entityStream) throws IOException, WebApplicationException {
+        return objectMapper.readValue(entityStream, type);
+    }
+
+    @Override
+    public void writeTo(Object o, Class aClass, Type type, Annotation[] annotations, MediaType mediaType, MultivaluedMap multivaluedMap, OutputStream outputStream) throws IOException, WebApplicationException {
+        outputStream.write(objectMapper.writeValueAsBytes(o));
+    }
+
+    @Override
+    public long getSize(Object o, Class aClass, Type type, Annotation[] annotations, MediaType mediaType) {
+        return -1;
+    }
+}
